6b3a728648dabf64f9479dcfbc3fbbfd4b8e094c
mayank6725 <69050987+mayank6725@users.noreply.github.com>
2021-03-20 00:00:05 +0530

    added ide and login component

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Dashboard from'../components/dashboard.vue'
+import login from'../components/login.vue'
 
 Vue.use(VueRouter);
 
@@ -9,7 +10,11 @@ const routes=[
     {
        path:'/',
        component:Dashboard
-    }
+    },
+    {
+        path:'/login',
+        component:login
+     }
 ];
 
 const router = new VueRouter({
